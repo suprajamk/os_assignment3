a9a21ad6b73d6176b20a83963ae6f19f464266a0
suprajamk <suprajamk@gmail.com>
2018-04-06 14:35:52 +0800

    read and write commit

--- e0210391_dev.c
+++ e0210391_dev.c
@@ -33,11 +33,32 @@ return 0; // always successful
 }
 ssize_t onebyte_read(struct file *filep, char *buf, size_t count, loff_t *f_pos)
 {
-/*please complete the function on your own*/
+	if (*f_pos == 0) {
+	// at position 0, need to use copy_to_user
+	// because kernel space and user space
+	// are different
+		copy_to_user(buf, onebyte_data, 1);
+	// advance the f_pos
+		*f_pos += 1;
+	// return 1, since the device only have 1 byte
+		return 1;
+	} else {
+		return 0;
+	}
 }
 ssize_t onebyte_write(struct file *filep, const char *buf, size_t count, loff_t *f_pos)
 {
-/*please complete the function on your own*/
+	if (*f_pos == 0) {
+	// Use copy_from_user due to pointer are in different
+	// address space
+		copy_from_user(onebyte_data, buf, 1);
+	// advance f_pos
+		*f_pos+=1;
+		return 1;
+	} else {
+	// no more space to write
+		return -ENOSPC;
+	}
 }
 static int onebyte_init(void)
 {
